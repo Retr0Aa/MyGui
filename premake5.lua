04b51127acd52fe9b7c153d0322d6693637a180e
Alexander Buchkov <retr0aalex@gmail.com>
2024-02-05 12:08:00 +0200

    Added Premake Support

---
+++ premake5.lua
@@ -0,0 +1,25 @@
+workspace "MyGui"
+   configurations { "Debug", "Release" }
+   startproject "Example"
+
+  project "MyGui"
+    kind "SharedLib"
+    language "C#"
+    location "MyGui"
+    targetdir "bin/MyGui/%{cfg.buildcfg}"
+
+    nuget { "Control.Draggable:1.0.5049.269" }
+
+    files { "MyGui/src/**.cs" }
+    
+    links { "System", "System.Drawing", "System.Windows.Forms" }
+
+  project "Example"
+    kind "ConsoleApp"
+    language "C#"
+    location "Example"
+    targetdir "bin/Example/%{cfg.buildcfg}"
+
+    files { "Example/src/**.cs" }
+    
+    links { "myGui", "System", "System.Drawing", "System.Windows.Forms" }
